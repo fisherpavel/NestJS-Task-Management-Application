d5a9b8221b1467995d93271573576eab39d89765
fisherpavel <fisherpavel@bk.ru>
2020-06-20 10:05:17 +0700

    add security hashing password from sign up

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,16 +2,19 @@ import { Repository, EntityRepository } from "typeorm"
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcrypt'
 
 @EntityRepository(User)
 export class UserRepositiry extends Repository<User> {
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
         const {username, password} = authCredentialsDto
 
-       
+
         const user = new User()
         user.username = username
-        user.password = password
+        user.salt = await bcrypt.genSalt()
+        user.password = await this.hashPassword(password, user.salt)
+
 
         try{
             await user.save()
@@ -22,8 +25,11 @@ export class UserRepositiry extends Repository<User> {
                 throw new InternalServerErrorException()
             }
         }
-        
     }
 
-
+    
+    private async hashPassword(password: string, salt: string): Promise<string>{
+            return bcrypt.hash(password, salt)
+    }
+    
 }
