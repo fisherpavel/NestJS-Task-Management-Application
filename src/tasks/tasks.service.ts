f04f9652d5b50e5df749920761b7e67236549cf7
fisherpavel <fisherpavel@bk.ru>
2020-06-22 10:56:27 +0700

    add tests from tasks.service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,13 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-
-
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskRepository } from './task.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 
 @Injectable()
 export class TasksService {
@@ -22,8 +20,6 @@ export class TasksService {
           ): Promise<Task[]>{
             return this.taskRepository.getTasks(filterDto, user)
         }
-
- 
 
         async getTaskById(
             id: number,
@@ -46,7 +42,6 @@ export class TasksService {
            return this.taskRepository.createTask(createTaskDto, user)
         }
 
-  
 
     async deleteTaskById(
         id: number,
@@ -57,9 +52,9 @@ export class TasksService {
 
     if(result.affected === 0){
         throw new NotFoundException(`Task with id ${id} not found`)
+        }
     }
-        
-    }
+
 
     async updateTaskStatus(
         id: number,
